7ea13da17be17073e5aa717631212250d2c99ff2
mickel_arroz <111148461+mickel-arroz@users.noreply.github.com>
2025-03-10 16:23:57 -0400

    Part 1.1 and 1.2

---
+++ part1/src/Content.jsx
@@ -0,0 +1,22 @@
+import Part from "./Part";
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part
+        name={props.part1}
+        exercises={props.exercises1}
+      />
+      <Part
+        name={props.part2}
+        exercises={props.exercises2}
+      />
+      <Part
+        name={props.part3}
+        exercises={props.exercises3}
+      />
+    </div>
+  );
+};
+
+export default Content;
---
+++ part1/src/Total.jsx
@@ -0,0 +1,11 @@
+const Total = (props) => {
+  return (
+    <>
+      <p>
+        <b>Number of exercises</b> {props.exercises}
+      </p>
+    </>
+  );
+};
+
+export default Total;
